8886b842b991bca478f046175ae3068c5d4067bb
azenizzka <azenizzka@gmail.com>
2024-12-05 20:51:22 +0700

    style: format project to Google style

--- main.cpp
+++ main.cpp
@@ -1,59 +1,50 @@
-﻿#include <vector>
-#include <functional>
+﻿#include <functional>
 #include <iostream>
+#include <vector>
 
 #include "tests.h"
 
-//массив всех тестов, который мы заполняем в функции initTests
+// массив всех тестов, который мы заполняем в функции initTests
 static std::vector<std::function<bool()>> tests;
 
-//тест 1
-bool test1()
-{
-  //пример какого-то теста
-  return 42 == (41 + 1); //passed
+// тест 1
+bool test1() {
+  // пример какого-то теста
+  return 42 == (41 + 1);  // passed
 }
 
-//тест 2
-bool test2()
-{
-  //пример какого-то теста
-  return 42 != (41 + 1); //failed
+// тест 2
+bool test2() {
+  // пример какого-то теста
+  return 42 != (41 + 1);  // failed
 }
 
-//тест 3
-bool test3()
-{
-  Candle candle{ 0.0, 3.0, 3.0, 3.0 };
+// тест 3
+bool test3() {
+  Candle candle{0.0, 3.0, 3.0, 3.0};
 
-  //пример какого-то теста
+  // пример какого-то теста
   return candle.high == 3.0;
 }
 
-void initTests()
-{
+void initTests() {
   tests.push_back(test1);
   tests.push_back(test2);
   tests.push_back(test3);
-  //tests.push_back(test4);
-  //tests.push_back(test5);
+  // tests.push_back(test4);
+  // tests.push_back(test5);
 }
 
-int launchTests()
-{
+int launchTests() {
   int total = 0;
   int passed = 0;
 
-  for (const auto& test : tests)
-  {
+  for (const auto& test : tests) {
     std::cout << "test #" << (total + 1);
-    if (test())
-    {
+    if (test()) {
       passed += 1;
       std::cout << " passed\n";
-    }
-    else
-    {
+    } else {
       std::cout << " failed\n";
     }
     total += 1;
@@ -61,12 +52,11 @@ int launchTests()
 
   std::cout << "\ntests " << passed << "/" << total << " passed!" << std::endl;
 
-  //0 = success
+  // 0 = success
   return total - passed;
 }
 
-int main()
-{
+int main() {
   initTests();
   return launchTests();
 }
