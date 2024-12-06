2e9f874f82b9b0a171e28847f7b3e5dca0a2ae86
azenizzka <azenizzka@gmail.com>
2024-12-06 18:06:52 +0700

    fix: include candle.h file to main.cpp, add template of test

--- main.cpp
+++ main.cpp
@@ -2,38 +2,16 @@
 #include <iostream>
 #include <vector>
 
-#include "tests.h"
+#include "candle.h"
 
-// массив всех тестов, который мы заполняем в функции initTests
 static std::vector<std::function<bool()>> tests;
 
-// тест 1
-bool test1() {
-  // пример какого-то теста
-  return 42 == (41 + 1);  // passed
+bool test_body_contains_1() {
+  //
+  return false;
 }
 
-// тест 2
-bool test2() {
-  // пример какого-то теста
-  return 42 != (41 + 1);  // failed
-}
-
-// тест 3
-bool test3() {
-  Candle candle{0.0, 3.0, 3.0, 3.0};
-
-  // пример какого-то теста
-  return candle.high == 3.0;
-}
-
-void initTests() {
-  tests.push_back(test1);
-  tests.push_back(test2);
-  tests.push_back(test3);
-  // tests.push_back(test4);
-  // tests.push_back(test5);
-}
+void initTests() { tests.push_back(test_body_contains_1); }
 
 int launchTests() {
   int total = 0;
@@ -52,7 +30,6 @@ int launchTests() {
 
   std::cout << "\ntests " << passed << "/" << total << " passed!" << std::endl;
 
-  // 0 = success
   return total - passed;
 }
 
